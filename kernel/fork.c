11f56b239421e7045be6d672513bc38e01443e2e
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-08 12:03:49 +0530

    kernel: Boost DDR bus for a short amount of time when zygote forks
    There is a gap from when a new app is forked to the time that the
    devfreq governor managing the DDR bus can pick up the increased load and
    ramp up appropriately. Since the polling interval for this governor is
    typically 50 ms, that means it may take up to 50 ms before the governor
    ramps up, which hurts cold launch app performance. Boosting the DDR bus
    for 50 ms upon a fork from zygote covers that gap, speeding up cold app
    launches.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,6 +95,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2209,6 +2210,10 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost DDR bus to the when userspace launches an app */
+	if (task_is_zygote(current))
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
