c21f410c6c5fd5fcb64819fe340277e3745a2d16
Suren Baghdasaryan <surenb@google.com>
2021-05-24 12:53:39 -0700

    BACKPORT: cgroup: make per-cgroup pressure stall tracking configurable
    PSI accounts stalls for each cgroup separately and aggregates it at each
    level of the hierarchy. This causes additional overhead with psi_avgs_work
    being called for each cgroup in the hierarchy. psi_avgs_work has been
    highly optimized, however on systems with large number of cgroups the
    overhead becomes noticeable.
    Systems which use PSI only at the system level could avoid this overhead
    if PSI can be configured to skip per-cgroup stall accounting.
    Add "cgroup_disable=pressure" kernel command-line option to allow
    requesting system-wide only pressure stall accounting. When set, it
    keeps system-wide accounting under /proc/pressure/ but skips accounting
    for individual cgroups and does not expose PSI nodes in cgroup hierarchy.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Link:  https://lore.kernel.org/patchwork/patch/1435705
    (cherry picked from commit 3958e2d0c34e18c41b60dc01832bd670a59ef70f
     https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git tj)
    
    Conflicts:
            include/linux/cgroup-defs.h
            kernel/cgroup/cgroup.c
    
    1. Trivial merge conflict in cgroup-defs.h due to missing CFTYPE_DEBUG
    2. Changed flags to (CFTYPE_NOT_ON_ROOT | CFTYPE_PRESSURE) in cgroup.c
    because in 4.19 psi files were allowed only in non-root cgroups.
    
    Bug: 178872719
    Bug: 191734423
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ifc8fbc52f9a1131d7c2668edbb44c525c76c3360
    (cherry picked from commit e9f038fec418d19926a8243163a5dd2334fe9dde)

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -6238,6 +6238,9 @@ static ssize_t show_delegatable_files(struct cftype *files, char *buf,
 		if (!(cft->flags & CFTYPE_NS_DELEGATABLE))
 			continue;
 
+		if ((cft->flags & CFTYPE_PRESSURE) && !cgroup_psi_enabled())
+			continue;
+
 		if (prefix)
 			ret += snprintf(buf + ret, size - ret, "%s.", prefix);
 
