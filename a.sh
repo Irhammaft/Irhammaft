e1ff3a4196297e9df9df31f9a6a757a3a5ac0777
venoom <venoomtulip@gmail.com>
2021-11-25 10:38:17 +0700

    add wakeloack fp

---
+++ a.sh
@@ -0,0 +1,31 @@
+!/bin/bash
+#
+# Compile script for QuicksilveR kernel
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+#ZIPNAME="venoomR-surya-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="/root/kernel/clang"
+GCC_64_DIR="/root/kernel/64"
+GCC_32_DIR="/root/kernel/32"
+DEFCONFIG="surya_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz dtbo.img
+
