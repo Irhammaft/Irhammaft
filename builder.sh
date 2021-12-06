da738b9d3f2e0c4f523b9cf1c43e1bf922ef857e
venoom <efaiza5@gmail.com>
2021-12-06 09:28:01 +0700

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -8,57 +8,38 @@ IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
 DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
 TANGGAL=${VERSION}-$(date +"%d%m%H%M")
 START=$(date +"%s")
-CLANG_VERSION=$(clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-KERNEL_DIR=$(pwd)
-PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+export VERSION=SIX
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_HOST="Venoom"
-export KBUILD_BUILD_USER="wartegCI"
+export KBUILD_BUILD_USER="WartegCI"
 export chat_id="-1001642575104"
 export DEF="surya_defconfig"
-export VERSION=SIX
-DEF_REG=0
+TC_DIR=${PWD}
+GCC64_DIR="${PWD}/gcc64"
+GCC32_DIR="${PWD}/gcc32"
+export PATH="$TC_DIR/bin/:$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH"
 BUILD_DTBO=1
 SIGN_BUILD=0
-INCREMENTAL=1
-
-if [ $INCREMENTAL = 0 ]
-then
-	make clean && make mrproper && rm -rf out && cd AnyKernel3/ && rm -rf * && git reset --hard && cd ..
-fi
-
-
-if [ $DEF_REG = 1 ]
-then
-make O=out ARCH=arm64 ${DEF}
-mv out/.config arch/arm64/configs/${DEF}
-		git add arch/arm64/configs/${DEF}
-		git commit -m "defconfig: Regenerate
-						This is an auto-generated commit"
-fi
-
-curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d text="Buckle up bois ${BRANCH} build has started" -d chat_id=${chat_id} -d parse_mode=HTML
 
 echo "CONFIG_PATCH_INITRAMFS=y" >> arch/arm64/configs/surya_defconfig
 
-make O=out ARCH=arm64 $DEF
-	make -j$(nproc --all) O=out \
-                      CC=clang \
-		      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=aarch64-linux-android- \
-                      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-		      LD=ld.lld \
-		      AS=llvm-as \
-		      AR=llvm-ar \
-                      NM=llvm-nm \
-                      OBJCOPY=llvm-objcopy \
-                      OBJDUMP=llvm-objdump \
-                      STRIP=llvm-strip 2>&1 | tee build.log
-		      
+curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d text="Buckle up bois ${BRANCH} build has started" -d chat_id=${chat_id} -d parse_mode=HTML
+
+   make O=out ARCH=arm64 $DEF
+       make -j$(nproc --all) O=out \
+				ARCH=arm64 \
+				CROSS_COMPILE_ARM32=arm-eabi- \
+				CROSS_COMPILE=aarch64-elf- \
+				AR=llvm-ar \
+				NM=llvm-nm \
+				OBJCOPY=llvm-objcopy \
+				LD=aarch64-elf-ld.lld 2>&1 | tee build.log
+
 END=$(date +"%s")
 DIFF=$((END - START))
+
 if [ -f $(pwd)/out/arch/arm64/boot/Image.gz ]
 	then
         if [ BUILD_DTBO = 1 ]
@@ -69,13 +50,13 @@ if [ -f $(pwd)/out/arch/arm64/boot/Image.gz ]
         fi
 # Post to CI channel
 curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d text="Branch: <code>$(git rev-parse --abbrev-ref HEAD)</code>
-Compiler Used : <code>${CLANG_VERSION} </code>
+Compiler Used : <code>GCC aka Giga Chad Compiler</code>
 Latest Commit: <code>$(git log --pretty=format:'%h : %s' -1)</code>
 <i>Build compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</i>" -d chat_id=${chat_id} -d parse_mode=HTML
 #curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d text="Flash now else bun" -d chat_id=${chat_id} -d parse_mode=HTML
 
 cp $(pwd)/out/arch/arm64/boot/Image.gz $(pwd)/AnyKernel3
-cp $(pwd)/out/arch/arm64/boot/dtb.img $(pwd)/AnyKernel3
+cp $(pwd)/out/arch/arm64/boot/dtb.img $(pwd)/AnyKernel3/dtb.img
 
         if [ -f ${DTBO} ]
         then
@@ -83,7 +64,8 @@ cp $(pwd)/out/arch/arm64/boot/dtb.img $(pwd)/AnyKernel3
         fi
 
         cd AnyKernel3
-        zip -r9 Venoom-surya-${TANGGAL}.zip * --exclude *.jar
+        make normal
+        ZIP_FINAL=$(echo *.zip)
 
         if [ SIGN_BUILD = 1 ]
         then
@@ -99,7 +81,7 @@ cp $(pwd)/out/arch/arm64/boot/dtb.img $(pwd)/AnyKernel3
 
         curl -F chat_id="${chat_id}"  \
                     -F caption="sha1sum: $(sha1sum Ven*.zip | awk '{ print $1 }')" \
-                    -F document=@"$(pwd)/Venoom-surya-${TANGGAL}.zip" \
+                    -F document=@"$ZIP_FINAL" \
                     https://api.telegram.org/bot${TOKEN}/sendDocument
 	fi
 
