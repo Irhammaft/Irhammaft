beb1a8b8ae048dc4d64694ad9fe5f5e8db9d0db7
venoom <venoomtulip@gmail.com>
2021-12-09 00:14:14 +0700

    Revert "cpu_input_boost: Ensure that we are using EAS while screen off"
    This reverts commit 32ab6081f50ba0defbb9f2115a22784c1d16d527.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -226,8 +226,6 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
 		policy->min = policy->cpuinfo.min_freq;
-		policy->min = get_idle_freq(policy);
-		sysctl_sched_energy_aware = 1;
 		return NOTIFY_OK;
 	}
 
@@ -247,7 +245,6 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	else
 		policy->min = policy->cpuinfo.min_freq;
 
-	/* If we are not boosting max for app launch/device wake, enable EAS */
 	sysctl_sched_energy_aware = 1;
 
 	return NOTIFY_OK;
