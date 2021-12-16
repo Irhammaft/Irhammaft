63e123192dad54ea39bc644efece1497185be344
venoom <venoomtulip@gmail.com>
2021-12-16 21:56:08 +0700

    fixed binder build

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4684,12 +4684,6 @@ static void binder_free_proc(struct binder_proc *proc)
 {
 	BUG_ON(!list_empty(&proc->todo));
 	BUG_ON(!list_empty(&proc->delivered_death));
-	device = container_of(proc->context, struct binder_device, context);
-	if (refcount_dec_and_test(&device->ref)) {
-		kfree(proc->context->name);
-		kfree(device);
-	}
-	WARN_ON(proc->outstanding_txns);
 	binder_alloc_deferred_release(&proc->alloc);
 	put_task_struct(proc->tsk);
 	put_cred(proc->cred);
