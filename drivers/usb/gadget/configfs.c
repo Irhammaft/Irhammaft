a69a09de8f276f3c4c6d4649a3284ee9f41f5583
root <root@K-I-N.localdomain>
2022-02-20 20:02:42 +0700

    Revert "Update configfs.c"
    This reverts commit a5c1fd9814741104c7f2bca579ea81ed02ed1338.

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -150,6 +150,7 @@ struct gadget_config_name {
 
 #define MAX_USB_STRING_LEN	126
 #define MAX_USB_STRING_WITH_NULL_LEN	(MAX_USB_STRING_LEN+1)
+#define USB_MAX_STRING_WITH_NULL_LEN	(USB_MAX_STRING_LEN+1)
 
 static int usb_string_copy(const char *s, char **s_copy)
 {
@@ -158,6 +159,7 @@ static int usb_string_copy(const char *s, char **s_copy)
 	char *copy = *s_copy;
 	ret = strlen(s);
 	if (ret > MAX_USB_STRING_LEN)
+	if (ret > USB_MAX_STRING_LEN)
 		return -EOVERFLOW;
 
 	if (copy) {
@@ -168,6 +170,11 @@ static int usb_string_copy(const char *s, char **s_copy)
 			return -ENOMEM;
 	}
 	strlcpy(str, s, MAX_USB_STRING_WITH_NULL_LEN);
+		str = kmalloc(USB_MAX_STRING_WITH_NULL_LEN, GFP_KERNEL);
+		if (!str)
+			return -ENOMEM;
+	}
+	strcpy(str, s);
 	if (str[ret - 1] == '\n')
 		str[ret - 1] = '\0';
 	*s_copy = str;
